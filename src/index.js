83a9cddba326c115e1b0af7f7a24aa5febc7e3bf
Jordan Mitchell <mordanjitchell@gmail.com>
2017-01-19 10:08:28 -0600

    JordanMitchell_FirstReactApp
    Basic Hello World react application

--- src/index.js
+++ src/index.js
@@ -1,7 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import Hello from './Hello';
+// class Hello extends React.Component {
+//   render() {
+//     return <h1>Hello {this.props.name}!</h1>;
+//   }
+// }
+//
+class App extends React.Component {
+  render() {
+    const users = [
+        {_id: 1, name: "Leia"},
+        {_id: 2, name: "Luke"},
+        {_id: 3, name:"Han"}
+    ];
+
+    return (
+      <div>{
+        users.map(function(user){
+          return <Hello key = {user._id} name = {user.name} />;
+        })
+      }
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
   <App />,
